b79d9f8fae56ff8356da8cf05ff42c33e8c98138
Mike Wilkes <mwwilkes@gmail.com>
2022-07-03 07:50:21 -0700

    fixes functionality, meets project requirements (hopefully)

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,45 @@
 
 window.onload = function () {
   const js = document.getElementById('js')
+  const cSharp = document.getElementById('csharp')
   const fortran = document.getElementById('fortran')
-  js.style.display = "none";
-  fortran.style = 'display:none';
+  js.style = "display:none";
+  cSharp.style.display = "none";
+  fortran.style.display = "none";
 
   document.querySelector("form").onsubmit = function (event) {
-    // hideResults();
     event.preventDefault();
-    const use = document.querySelector("input[name='use']:checked").value;
-    // const fortran = document.querySelector("input[name='fortran']:checked").value;
 
+    const use = document.querySelector("input[name='use']:checked").value;
+   
     if (use === "yes") {
       js.style = "display:block";
+      cSharp.style = "display:none";
+      fortran.style = "display:none";
+    } else if (use === "no") {
+      cSharp.style = "display:block";
+      js.style = "display:none";
+      fortran.style = "display:none";
+    }  else if (use === "sec-yes") {
+      fortran.style = "display:block";
+      js.style = "display:none";
+      cSharp.style = "display:none";
     }
+    // const sec = document.querySelector("input[name='sec']:checked").value;
+
+   
+
     // click listener that runs hid results 
+
+    // const bttn = document.getElementById('bttn');
+
+// const form = document.getElementById('questions');
+
+// form.addEventListener('submit', function handleSubmit(event) {
+//   event.preventDefault();
+//   form.reset();
+// });
+// handleSubmit();
+
   };
 };
