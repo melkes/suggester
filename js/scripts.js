2a7e768af6fb07b0c38b5e9693ad8212e5779bfb
Mike Wilkes <mikewilkes@outlook.com>
2022-11-06 06:06:28 -0800

    completed js and add styling tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,51 @@
 //hide results
 window.onload = function () {
-  const js = document.getElementById('js')
+  const html = document.getElementById('html')
   const cSharp = document.getElementById('csharp')
   const python = document.getElementById('python')
-  js.style = "display:none";
+  const fortran = document.getElementById('fortran')
+  html.style = "display:none";
   cSharp.style = "display:none";
-  python.style = "display:none"
+  python.style = "display:none";
+  fortran.style = "display:none";
 
-  
+  document.querySelector("form").onsubmit = function (event) {
+    event.preventDefault();
+    
+    const quiz = document.getElementById('quiz')
+    let webDev = document.querySelector("input[name='question1']:checked").value;
+    let data = document.querySelector("input[name='data']:checked").value;
+    let sec = document.querySelector("input[name='sec']:checked").value;
+    if (webDev === "yes") {
+      html.style = "display:block";
+      cSharp.style = "display:none";
+      python.style = "display:none";
+      fortran.style = "display:none";
+      quiz.style = "display:none";
+
+    } else if (sec === "sec-yes") {
+      html.style = "display:none";
+      cSharp.style = "display:none";
+      python.style = "display:none";
+      fortran.style = "display:block";
+      quiz.style = "display:none";
+
+
+    } else if (data === "yes") {
+      html.style = "display:none";
+      cSharp.style = "display:none";
+      python.style = "display:yes";
+      fortran.style = "display:none";
+      quiz.style = "display:none";
+
+    } else {
+      html.style = "display:none";
+      cSharp.style = "display:block";
+      python.style = "display:none";
+      fortran.style = "display:none";
+      quiz.style = "display:none";
+
+    }
+
+};
 };
