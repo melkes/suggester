2f8e585653ef7f547fba8e94445c4c0fc071f1a1
Mike Wilkes <mikewilkes@outlook.com>
2022-11-06 04:03:08 -0800

    added some placeholder questions and part of necessary js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//hide results
+window.onload = function () {
+  const js = document.getElementById('js')
+  const cSharp = document.getElementById('csharp')
+  const python = document.getElementById('python')
+  js.style = "display:none";
+  cSharp.style = "display:none";
+  python.style = "display:none"
+
+  
+};
