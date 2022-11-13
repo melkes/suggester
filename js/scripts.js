3f94f2f4fb4fdcca7db6d623862bb701a11b3390
Mike Wilkes <mikewilkes@outlook.com>
2022-11-13 03:32:50 -0800

    WIP more working function for reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
-//hide results
+function resetResults() {
+  document.getElementById('quiz').style = "display:block";
+  document.getElementById('html').style = "display:none";
+  document.getElementById('csharp').style = "display:none";
+  document.getElementById('python').style = "display:none";
+  document.getElementById('fortran').style = "display:none";
+   };
+ //hide results
 window.onload = function () {
   const html = document.getElementById('html')
   const cSharp = document.getElementById('csharp')
   const python = document.getElementById('python')
   const fortran = document.getElementById('fortran')
+  const quiz = document.getElementById('quiz')
+
   html.style = "display:none";
   cSharp.style = "display:none";
   python.style = "display:none";
   fortran.style = "display:none";
 
-
   document.querySelector("form").onsubmit = function (event) {
     event.preventDefault();
     
-    const quiz = document.getElementById('quiz')
     let webDev = document.querySelector("input[name='question1']:checked").value;
     let data = document.querySelector("input[name='data']:checked").value;
     let sec = document.querySelector("input[name='sec']:checked").value;
@@ -50,14 +57,28 @@ window.onload = function () {
 
 };
 
+
+
+
+// function hideQuiz() {
+//   quiz.style = "display:none";
+//   };
+
+// const btn = document.querySelector("button");  
+// function resetQ (event) {
+
+//  quiz.style = "display:yes";
+// }
+// btn.onclick = resetQ();
 };
 
-function resetFunc(event) {
 
-  document.querySelector("#reset").onsubmit
-const reset = document.getElementById('#reset')
-console.log(resetFunc)
-}
+
+// document.querySelector("#reset").on = function (event) {
+//   event.preventDefault();
+// const reset = document.getElementById('reset')
+// console.log('reset')
+// }
 
 // function hideResults() {
 //   const reset = document.querySelector('#reset')
